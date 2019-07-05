680fce793c00f002d18095d85b97247e51717e90
Cameron <hagaboogaplease@gmail.com>
2019-07-04 17:19:14 -0700

    sd

--- a4.hs
+++ a4.hs
@@ -2,7 +2,7 @@ data Token = Num Double
     | Op String
     deriving (Show, Eq)
     
-inc :: Token -> Double
-inc (Num x) = x + 1
+inc :: Num -> Double
+inc x = x + 1
 
 main = print $ inc (Num 1)
