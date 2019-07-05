1112a17a62c6741b8e291c1b6cd29347a5f0bdd4
hagabooga <hagaboogaplease@gmail.com>
2019-07-05 03:09:27 -0700

    sdasd

--- a4.hs
+++ a4.hs
@@ -1,3 +1,4 @@
+-- Token Defintion
 data Token = Empty
     | Num Double
     | Op String
@@ -19,11 +20,15 @@ is_err _ = False
 get_num :: Token -> Double
 get_num (Num x) = x
 
+-- Operator Logic
 inc :: [Token] -> [Token]
 inc [] = [Err "inc: empty stack"]
 inc (x:xs) 
     | is_num x = (Num $ (get_num x) + 1 ): xs
     | otherwise = Err "inc: arg not a Number" : xs
+
+dec :: [Token] -> [Token]
+
 
 plus :: [Token] -> [Token]
 plus [] = [Err "+: empty stack"]
@@ -31,11 +36,16 @@ plus (x:xs)
     | length (x:xs) < 2 = Err "+: not enough args" : xs
     | is_num x = Num( (get_num x) + (get_num (xs!!0))) : tail xs
 
+
+
+
+-- Op Token to Function
 find_op :: Token -> ([Token] -> [Token])
 find_op x
     | x == Op "inc" = inc
     | x == Op "+" = plus
 
+-- Calculate Stack
 start_calc_stack :: ([Token], [Token]) -> ([Token], [Token])
 start_calc_stack ([],(y:ys)) 
     | is_op y = start_calc_stack (find_op y $ [], (ys)) 
@@ -46,9 +56,10 @@ start_calc_stack ((x:xs), (y:ys))
     | is_op y = start_calc_stack  (find_op y $ (x:xs), ys)
     | otherwise = start_calc_stack (y:(x:xs), ys)
 
+-- String to Token
 tokenize :: String -> Token
-tokenize x 
-    | or [x == "inc", x == "dec", x == "sqrt", x == "sin", x == "cos", x == "inv", x == "+"] = Op x
+tokenize x
+    | or (map (\z -> x == z) ["inc","dec","sqrt","sin","cos","inv","+","*","-","/","+all","*all","dup","pop","clear","swap"]) = Op x
     | otherwise = Num (read x :: Double)
 
 calcStack :: String -> String
